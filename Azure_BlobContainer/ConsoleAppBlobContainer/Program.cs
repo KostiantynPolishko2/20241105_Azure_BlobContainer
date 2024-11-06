4b9d24c2c9b2448b9fbcb122ecf040e78a77fb6c
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-06 16:21:28 +0200

    3 Managed logic to download files from BlobContainer of AzureStorage

--- Azure_BlobContainer/ConsoleAppBlobContainer/Program.cs
+++ Azure_BlobContainer/ConsoleAppBlobContainer/Program.cs
@@ -11,40 +11,85 @@ Console.WriteLine("Azure Storage and Blob Container!\n");
 //copy connection string from section Access Key of Storage account
 string connectionString = "";
 
-//connection to azure storage
+//connection to Azure storage
 BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-Console.WriteLine($"stotage:\t{blobServiceClient.AccountName}");
+Console.WriteLine($"storage:\t{blobServiceClient.AccountName}");
+
+
+//=================================LIST OF CONTAINERS=================================//
+
+//get list of blobContainers from Azure storage
+Pageable<BlobContainerItem> containerItems = blobServiceClient.GetBlobContainers();
+Console.WriteLine("\nBloBContainerItems:");
+foreach (BlobContainerItem item in containerItems)
+{
+    if (item.IsDeleted != null)
+        continue;
+    Console.WriteLine($"\t{item.Name}");
+}
 
 //creation of container on azure storage
 string containerName = "files";
-BlobContainerClient containerClient = null!;
+//BlobContainerClient blobContainerClient = null!;
 //containerClient = blobServiceClient.CreateBlobContainer(containerName);
 
-//get blob containerClient by its' name
-containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-Console.WriteLine($"blob container:\t{containerClient.Name}\n");
-
-//get blobClient instance for download and upload files (FIO) in blob container
-string blobItemName = "documents";
-BlobClient blobClient = containerClient.GetBlobClient(blobItemName);
-
-//get list of blobItems
-Pageable<BlobItem> blobItems = containerClient.GetBlobs();
+//get list of blobItems (files inside blob container)
+Pageable<BlobItem> blobItems = blobServiceClient.GetBlobContainerClient(containerName).GetBlobs();
+Console.WriteLine("\nBlobItem:");
 foreach (BlobItem item in blobItems)
 {
     if (item.Deleted)
         continue;
-    Console.WriteLine($"BlobItem:\t{item.Name}");
+    Console.WriteLine($"\t{item.Name}");
 }
 BlobItem? blobItem = blobItems.FirstOrDefault(b => b.Equals("files"));
 
-//get list of blob containers from Azure storage
-Pageable<BlobContainerItem> containerItems = blobServiceClient.GetBlobContainers();
-foreach (BlobContainerItem item in containerItems)
+//=================================FILE FIO (DOWNLOAD / UPLOAD)=================================//
+
+//get blob containerClient by its' name
+string blobContainer = "files";
+BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainer);
+Console.WriteLine($"\nblob container:\t{blobContainerClient.Name}");
+
+//get blobClient instance for download and upload files (FIO) in blob container
+foreach(var item in blobItems)
 {
-    if(item.IsDeleted != null) 
+    if (item.Deleted)
         continue;
-    Console.WriteLine($"BloBContainerItem:\t{item.Name}");
+    else if (item.Name.Contains("/"))
+        continue;
+
+    //get client over file in container
+    BlobClient blobClient = blobContainerClient.GetBlobClient(item.Name);
+    //create new path of filestream for download selected file
+    string downloadPath = $"../../../AzureBlobs/{item.Name}";
+
+    try
+    {
+        using (FileStream fileStream = File.OpenWrite(downloadPath))
+        {
+            blobClient.DownloadTo(fileStream);
+        }
+        Console.WriteLine($"downloded file:\t{item.Name}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{item.Name} did not download");
+    }
+
 }
+
+//example to download one file
+//
+//string blobItemName = blobItems.ToArray()[1].Name;
+//BlobClient blobClient = blobContainerClient.GetBlobClient(blobItemName);
+
+//string downloadFilePath = $"../../../AzureBlobs/downlod_{blobItemName}";
+//FileStream fileStream = File.OpenWrite(downloadFilePath);
+//blobClient.DownloadTo(fileStream);
+
+//blobClient.Delete(DeleteSnapshotsOption.IncludeSnapshots);
+//Console.WriteLine($"downloded file:\t{blobItemName}");
+//fileStream.Close();
 
 Console.WriteLine("\nend");
